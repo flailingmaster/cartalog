74fe6a2e687fbfd4d0abb2f569ed2dda92d0230a
Raymond Wang <raymond@blinkingtwelve.net>
2016-09-07 23:27:49 -0700

    reorganized functions and output

--- app/Console/Commands/SearchCars.php
+++ app/Console/Commands/SearchCars.php
@@ -34,75 +34,33 @@ class SearchCars extends Command {
     public function handle()
     {
         $this->info('it works!');
-        $driver = RemoteWebDriver::create("http://localhost:9515", DesiredCapabilities::chrome(), 90 * 1000, 90 * 1000);
-        $driver->get($this->searchstring1);
-        $from = $driver->findElements(WebDriverBy::cssSelector(
-          'div.listing-row__details'));
-        foreach ($from as $row) {
-          $title = $row->findElement(WebDriverBy::cssSelector(
-          	'h2.cui-delta.listing-row__title'));
-          $price = $row->findElement(WebDriverBy::cssSelector(
-            'span.listing-row__price'));
-          $mileage = $row->findElement(WebDriverBy::cssSelector(
-            'span.listing-row__mileage'));
-          //mileage: listing-row__mileage
-
-
-          $this->info("title:".$title->getText());
-          $this->info("price:".$price->getText());
-          $this->info("mileage:".$mileage->getText());
-        }
-
-        //$element = $this->webdriver_test($this->searchstring1);
-        //$this->info($element->getText());
-        /*$client = new Client();
-        $crawler = $client->request('GET', $this->searchstring2);
-        $crawler->filter('body')->each(function ($node) {
-          $text = print_r($node, TRUE);
-          $this->info("$text \n");
-        });*/
+        $cars = $this->webdriver_test($this->searchstring1);
+        $this->table(["title", "price", "mileage"], $cars);
     }
 
     public function webdriver_test($url)
     {
       // https://github.com/yatnosudar/PHP-WebDriver-Phantomjs-Selenium/blob/master/example.php
-      // start Firefox
+
       $host = 'http://127.0.0.1:4444/wd/hub'; // this is the default
-      /*
-      $capabilities = array(
-      	WebDriverCapabilityType::BROWSER_NAME => 'phantomjs',
-      	WebDriverCapabilityType::ACCEPT_SSL_CERTS=> true,
-      	WebDriverCapabilityType::JAVASCRIPT_ENABLED=>true);
-      $desired_capabilities = DesiredCapabilities::firefox();
-      $desired_capabilities->setCapability('acceptSslCerts', false);
-      $desired_capabilities->setCapability('javascriptEnabled', true);
-      $driver = RemoteWebDriver::create($host, $desired_capabilities, 90 * 1000, 90 * 1000 );
-      $driver = new RemoteWebDriver($host, $capabilities);
-      // navigate to 'http://docs.seleniumhq.org/'
-      $session = $driver->get($url);
-      // Search 'php' in the search box
-      $from = $driver->findElement(WebDriverBy::cssSelector(
-      	'h2.cui-delta.listing-row__title'));
-      $from->click();
-*/
 
-
-//      $driver = RemoteWebDriver::create($host, DesiredCapabilities::firefox(), 90 * 1000, 90 * 1000);
-/*
-      $driver->get($url);
-      $from = $driver->findElement(WebDriverBy::cssSelector(
-        'h2.cui-delta.listing-row__title'));
-      return $from;
-*/
       $driver = RemoteWebDriver::create("http://localhost:9515", DesiredCapabilities::chrome(), 90 * 1000, 90 * 1000);
       $driver->get($url);
       $from = $driver->findElements(WebDriverBy::cssSelector(
-        'listing-row__details'));
-      return $from;
-      //$element = $driver->findElement(WebDriverBy::name('q'));
-      //$element->sendKeys('Cheese');
-      //$element->submit();
-//*/
+        'div.listing-row__details'));
+      $cars = array();
+      foreach ($from as $row) {
+        $title = $row->findElement(WebDriverBy::cssSelector(
+        	'h2.cui-delta.listing-row__title'));
+        $price = $row->findElement(WebDriverBy::cssSelector(
+          'span.listing-row__price'));
+        $mileage = $row->findElement(WebDriverBy::cssSelector(
+          'span.listing-row__mileage'));
+
+        $cars[] = [$title->getText(), $price->getText(), $mileage->getText()];
+      }
+      return $cars;
+
     }
 
 }
