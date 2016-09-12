4c7eb408c5a1417a9b8b9cf8d7366bf45bf52302
Raymond Wang <raymond@blinkingtwelve.net>
2016-09-11 23:05:40 -0700

    found links, can't click them.

--- app/Console/Commands/SearchCars.php
+++ app/Console/Commands/SearchCars.php
@@ -22,7 +22,7 @@ class SearchCars extends Command {
      *
      * @var string
      */
-     protected $searchstring1 = "https://www.cars.com/for-sale/searchresults.action?zc=85202&rd=99999&prMn=0&prMx=50000&yrId=27381&yrId=20201&yrId=20145&yrId=20200&yrId=20144&yrId=20199&mdId=20567&transTypeId=28112&mkId=20081&stkTypId=28881&sf1Nm=price&sf1Dir=DESC&sf2Nm=miles&sf2Dir=ASC&page=1&perPage=50&sortFeatures=buryUsedLowPrice&sortFeatures=buryNewLowPrice&sortFeatures=buryLowPriceOlderThanSix&sortFeatures=buryNoPrice&sortFeatures=buryUsedLowMileage&searchSource=GN_REFINEMENT";
+     protected $searchstring1 = "https://www.cars.com/for-sale/searchresults.action?zc=85202&rd=99999&prMn=0&prMx=50000&yrId=27381&yrId=20201&yrId=20145&yrId=20200&yrId=20144&yrId=20199&mdId=20567&transTypeId=28112&mkId=20081&stkTypId=28881&sf1Nm=price&sf1Dir=DESC&sf2Nm=miles&sf2Dir=ASC&page=1&perPage=100&sortFeatures=buryUsedLowPrice&sortFeatures=buryNewLowPrice&sortFeatures=buryLowPriceOlderThanSix&sortFeatures=buryNoPrice&sortFeatures=buryUsedLowMileage&searchSource=GN_REFINEMENT";
      protected $searchstring2 = "https://www.cars.com/for-sale/searchresults.action?zc=85202&rd=99999&prMn=0&prMx=50000&yrId=27381&yrId=20201&yrId=20145&yrId=20200&yrId=20144&yrId=20199&mdId=20567&transTypeId=28112&mkId=20081&stkTypId=28881&sf1Nm=price&sf1Dir=DESC&sf2Nm=miles&sf2Dir=ASC&page=1&perPage=100&sortFeatures=buryUsedLowPrice&sortFeatures=buryNewLowPrice&sortFeatures=buryLowPriceOlderThanSix&sortFeatures=buryNoPrice&sortFeatures=buryUsedLowMileage&searchSource=UTILITY";
     protected $description = 'Search and Store Interesting Cars for Sale';
 
@@ -46,12 +46,31 @@ class SearchCars extends Command {
 
       $driver = RemoteWebDriver::create("http://localhost:9515", DesiredCapabilities::chrome(), 90 * 1000, 90 * 1000);
       $driver->get($url);
+      $nextpages = $driver->findElements(WebDriverBy::cssSelector(
+        'ul.page-list'));
+      $this->info("next page for loop");
+      foreach ($nextpages as $next) {
+        $this->info("in next page for loop");
+        $test = $next->findElements(WebDriverBy::cssSelector('a'));
+        foreach ($test as $links) {
+         $this->info($links->getText());  
+        }
+
+      }
+      $cars = $this->parse_page($driver);
+      return $cars;
+
+
+    }
+
+    public function parse_page($driver)
+    {
       $from = $driver->findElements(WebDriverBy::cssSelector(
         'div.listing-row__details'));
       $cars = array();
       foreach ($from as $row) {
         $title = $row->findElement(WebDriverBy::cssSelector(
-        	'h2.cui-delta.listing-row__title'));
+          'h2.cui-delta.listing-row__title'));
         $price = $row->findElement(WebDriverBy::cssSelector(
           'span.listing-row__price'));
         $mileage = $row->findElement(WebDriverBy::cssSelector(
@@ -60,7 +79,6 @@ class SearchCars extends Command {
         $cars[] = [$title->getText(), $price->getText(), $mileage->getText()];
       }
       return $cars;
-
     }
 
 }
