3692646b6eb63a843598229853cee55e68a2ce49
Raymond Wang <raymond@blinkingtwelve.net>
2016-09-16 23:45:40 -0500

    adding a model for external links

---
+++ database/migrations/2016_09_14_225000_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->string('comment');
+            $table->mediumText('payload');
+            $table->string('link');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('links');
+    }
+}
